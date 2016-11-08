9b592b87deef99bed295a574bfedb9e0fdafe846
shaiendra <sshail@sapient.com>
2016-11-08 17:09:46 +0000

    formating..

--- src/main/java/com/mapr/examples/Consumer.java
+++ src/main/java/com/mapr/examples/Consumer.java
@@ -15,8 +15,8 @@ import java.util.Properties;
 import java.util.Random;
 
 /**
- * This program reads messages from two topics. Messages on "shailTest" are analyzed
- * to estimate latency (assuming clock synchronization between producer and consumer).
+ * This program reads messages from two topics. Messages on "shailTest" are analyzed to estimate
+ * latency (assuming clock synchronization between producer and consumer).
  * <p/>
  * Whenever a message is received on "slow-messages", the stats are dumped.
  */
@@ -46,47 +46,42 @@ public class Consumer {
             if (records.count() == 0) {
                 timeouts++;
             } else {
-                try{
+                try {
                     System.out.printf("Got %d records after %d timeouts\n", records.count(), timeouts);
-                }catch(Exception e){}
+                } catch (Exception e) {
+                }
                 timeouts = 0;
             }
             for (ConsumerRecord<String, String> record : records) {
                 switch (record.topic()) {
                     case "shailTest":
                         // the send time is encoded inside the message
-                        String value=record.value();
-                        try{
-                        JsonNode msg = mapper.readTree(value);
-                        switch (msg.get("type").asText()) {
-                            case "test":
-                                long latency = (long) ((System.nanoTime() * 1e-9 - msg.get("t").asDouble()) * 1000);
-                                stats.recordValue(latency);
-                                global.recordValue(latency);
-                                break;
-                            case "marker":
-                                // whenever we get a marker message, we should dump out the stats
-                                // note that the number of fast messages won't necessarily be quite constant
-                                
-                                System.out.printf("%d messages received in period, latency(min, max, avg, 99%%) = %d, %d, %.1f, %d (ms)\n",
-                                        stats.getTotalCount(),
-                                        stats.getValueAtPercentile(0), stats.getValueAtPercentile(100),
-                                        stats.getMean(), stats.getValueAtPercentile(99));
-                                System.out.printf("%d messages received overall, latency(min, max, avg, 99%%) = %d, %d, %.1f, %d (ms)\n",
-                                        global.getTotalCount(),
-                                        global.getValueAtPercentile(0), global.getValueAtPercentile(100),
-                                        global.getMean(), global.getValueAtPercentile(99));
-                                
-                                stats.reset();
-                                break;
-                            default:
-                                throw new IllegalArgumentException("Illegal message type: " + msg.get("type"));
-                        }
-                        }catch(Exception e){
+                        String value = record.value();
+                        try {
+                            JsonNode msg = mapper.readTree(value);
+                            switch (msg.get("type").asText()) {
+                                case "test":
+                                    long latency = (long) ((System.nanoTime() * 1e-9 - msg.get("t").asDouble()) * 1000);
+                                    stats.recordValue(latency);
+                                    global.recordValue(latency);
+                                    break;
+                                case "marker":
+                                    // whenever we get a marker message, we should dump out the stats
+                                    // note that the number of fast messages won't necessarily be quite constant
+
+                                    System.out.printf("%d messages received in period, latency(min, max, avg, 99%%) = %d, %d, %.1f, %d (ms)\n", stats.getTotalCount(), stats.getValueAtPercentile(0), stats.getValueAtPercentile(100), stats.getMean(), stats.getValueAtPercentile(99));
+                                    System.out.printf("%d messages received overall, latency(min, max, avg, 99%%) = %d, %d, %.1f, %d (ms)\n", global.getTotalCount(), global.getValueAtPercentile(0), global.getValueAtPercentile(100), global.getMean(), global.getValueAtPercentile(99));
+
+                                    stats.reset();
+                                    break;
+                                default:
+                                    throw new IllegalArgumentException("Illegal message type: " + msg.get("type"));
+                            }
+                        } catch (Exception e) {
                             System.out.println(value);
                         }
                         break;
-                        
+
                     case "summary-markers":
                         break;
                     default:
